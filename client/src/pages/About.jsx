b30c79154bf68572c853a0a89c7a736d0b87c0b4
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-15 17:37:18 -0800

    commented routes out to make start command run and making edits for about page. giving it information on devs and about the company

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,14 +1,33 @@
-
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 export default function About(){
     return(
         <div>
             <h1>About</h1>
+            <p>Our mission is to connect people who love cooking and inspire them to explore new cuisines, ingredients, and techniques. Whether you are a beginner or a master chef, you can find something to suit your taste buds and skill level on our platform.</p>
+                <br/>
+            <p>We believe that cooking is not only a way to nourish our bodies, but also a way to express our creativity, culture, and personality. That’s why we encourage our users to interact with each other, exchange feedback, and collaborate on new recipes. You can also customize any recipe to your liking, or create your own from scratch using our easy-to-use recipe builder.</p>
             <p>This is a team consists of 4 developers.</p>
             <ul>
-                <li>Gabe Velasquez</li>
-                <li>Chuyi Lee</li>
-                <li>Luke Hevey</li>
-                <li>Raul Valverde</li>
+                <li>
+                    Gabe Velasquez<a href="https://github.com/Gabe-Velasquez">
+                    <FontAwesomeIcon icon={faGithub} />
+                </a>
+                </li>
+                <li>
+                    Chuyi Lee
+                    <a href="https://github.com/Achurale">
+                    <FontAwesomeIcon icon={faGithub}/></a>
+                </li>
+                <li>Luke Hevey
+                    <a href="https://github.com/lukehevey">
+                    <FontAwesomeIcon icon={faGithub} /></a>
+                </li>
+                <li>
+                    Raul Valverde
+                    <a href="https://github.com/Hexteezy">
+                    <FontAwesomeIcon icon={faGithub} /></a>
+                </li>
             </ul>
         </div>
     )
--- server/server.js
+++ server/server.js
@@ -6,7 +6,7 @@ const { authMiddleware } = require('./utils/auth');
 
 const { typeDefs, resolvers } = require('./schemas');
 const db = require('./config/connection');
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
