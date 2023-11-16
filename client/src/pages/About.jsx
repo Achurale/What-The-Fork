04d912f9798124e43ef3ff1146faaff548fb9e20
Gabe <gabriel.velasquez0116@gmail.com>
2023-11-15 18:00:19 -0800

    server fixes with start being available, changed to dev

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,7 +7,7 @@ export default function About(){
             <p>Our mission is to connect people who love cooking and inspire them to explore new cuisines, ingredients, and techniques. Whether you are a beginner or a master chef, you can find something to suit your taste buds and skill level on our platform.</p>
                 <br/>
             <p>We believe that cooking is not only a way to nourish our bodies, but also a way to express our creativity, culture, and personality. That’s why we encourage our users to interact with each other, exchange feedback, and collaborate on new recipes. You can also customize any recipe to your liking, or create your own from scratch using our easy-to-use recipe builder.</p>
-            <p>This is a team consists of 4 developers.</p>
+            <p>This is a team consists of 4 developers: </p>
             <ul>
                 <li>
                     Gabe Velasquez<a href="https://github.com/Gabe-Velasquez">
