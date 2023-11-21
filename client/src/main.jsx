87cb6e9e9ae2fe1023d324115bf620e572056858
lukehevey <lukehevey8@gmail.com>
2023-11-20 19:08:38 -0600

    fixed main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -45,3 +45,48 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById('root')).render(
   <RouterProvider router={router} />
 )
+
+// import SearchRecipes from './pages/SearchRecipes'
+// import SavedRecipes from './pages/SavedRecipes'
+
+// import CreateRecipes from './pages/CreateRecipes.jsx'
+// import Profile from './pages/Profile.jsx'
+
+// import About from './pages/About';
+// import CreateRecipes from './pages/CreateRecipes.jsx'
+// import Profile from './pages/Profile.jsx'
+
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <App />,
+//     errorElement: <h1 className='display-2'>Wrong page!</h1>,
+//     children: [
+//       {
+//         index: true,
+//         element: <SearchRecipes />
+//       }, {
+//         path: '/saved',
+//         element: <SavedRecipes />
+//       },
+//       {
+//         path: '/create',
+//         element: <CreateRecipes />
+//       },
+//       {
+//         path: '/profile',
+//         element: <Profile />
+//       },
+//       {
+//         path: '/about',
+//         element: <About />
+//       },
+//     ]
+//   }
+// ])
+
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+// )
